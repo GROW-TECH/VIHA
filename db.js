ad910e3c304e604e8bb8b72ed43459b29bdd3e5b
vishnuselvan9608 <vishnu.slm1996@gmail.com>
2026-01-06 18:35:16 +0530

    add

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 import mysql from "mysql2/promise";
 
 export const pool = mysql.createPool({
-  host: "localhost",              // ✅ IMPORTANT
-  user: "growtechnologies_viha",   // cPanel DB user
-  password: "growtechnologies_viha",  // 🔴 NOT db name
-  database: "growtechnologies_viha",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: Number(process.env.DB_PORT),
   waitForConnections: true,
   connectionLimit: 10,
 });
